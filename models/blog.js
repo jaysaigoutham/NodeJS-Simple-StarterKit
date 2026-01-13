faa86d1f13553a1e77efaff9ece64eb307be4d5f
Jayasai Goutheman <gouthaman93@gmail.com>
2026-01-13 23:16:17 +0530

    Initial Commit

---
+++ models/blog.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 1
+  },
+  likes: {
+    type: Number,
+    required: true,
+    minlength: 1
+  },
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
---
+++ tests/reverse.test.js
@@ -0,0 +1,16 @@
+const { test } = require('node:test')
+const assert = require('node:assert')
+
+const reverse = require('./testing_methods').reverse
+
+test('reverse of a', () => {
+  const result = reverse('a')
+
+  assert.strictEqual(result, 'a')
+})
+
+test('reverse of react', () => {
+  const result = reverse('react')
+
+  assert.strictEqual(result, 'tcaer')
+})
