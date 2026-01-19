6dacab6dfe35a684796bbf71beb81bacc42d36a5
Jayasai Goutheman <gouthaman93@gmail.com>
2026-01-19 14:20:43 +0530

    Updating unit tests

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,11 @@
 const info = (...params) => {
-    console.log(...params)
+    //console.log(...params)
+    console.log("ENV (", process.env.NODE_ENV, ") : ", ...params)
 }
 
 const error = (...params) => {
-    console.log(...params)
+    //console.log(...params)
+    console.log("ENV (", process.env.NODE_ENV, ") : ", ...params)
 }
 
 module.exports = {info, error}
